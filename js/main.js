026e595801cdcdae29d6029e244a3267a7cc61e5
charlesamoss & Jordan Johansen <student@epicodus.com>
2015-09-10 11:21:01 -0700

    add finished app with frontend

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 var countBy = function(end, step) {
   var result = [];
-  for (var i = step; i <= end; i += step) {
-    result.push(i);
+
+  if (step > end) {
+    result.push('Great Sadness (first number must be larger than the second number');
+  } else {
+    for (var i = step; i <= end; i += step) {
+      result.push(i);
+    }
   }
+
   return result;
 };
+
+$(document).ready(function() {
+
+    $("form#countBy").submit(function(event) {
+
+        var end = parseInt($("input#end").val());
+        var step = parseInt($("input#step").val());
+
+        var result = countBy(end, step);
+
+        $("#result p").text(result.join(", "));
+
+        $("#result").show();
+        event.preventDefault();
+    });
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,8 @@ describe('countBy', function() {
   it("returns the output array of counts to first entered number", function() {
     expect(countBy(30,10)).to.eql([10, 20, 30]);
   });
+
+  it("returns error statment if users enters smaller first number", function() {
+    expect(countBy(10,50)).to.eql(['Great Sadness (first number must be larger than the second number']);
+  });
 });
